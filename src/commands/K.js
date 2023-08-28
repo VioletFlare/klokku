d332cd754b62e1300f9d54a760d6883f362cc8e5
vf <vf>
2023-08-28 18:50:16 +0200

    add islamic calendar

--- src/commands/K.js
+++ src/commands/K.js
@@ -1,3 +1,5 @@
+const { Temporal } = require('@js-temporal/polyfill');
+
 class K {
     constructor () {
         this.format = {
@@ -43,12 +45,54 @@ class K {
         return timeStrings;
     }
 
+    _fixNumber(number) {
+        let fixed = number;
+        
+        if (number < 10) {
+            fixed = `0${number}`;
+        }
+
+        return fixed;
+    }
+
+    _formatDateTemporalToString(date) {
+        const day = this._fixNumber(date.day);
+        const month = this._fixNumber(date.month);
+        const hour = this._fixNumber(date.hour);
+        const minute = this._fixNumber(date.minute);
+        const second = this._fixNumber(date.second);
+
+        return `${day}/${month}/${date.year}, ${hour}:${minute}:${second}`;
+    }
+
+    _getTimeStringsIslamic() {
+        const tzPK = Temporal.TimeZone.from('Asia/Karachi');
+        const tzIT = Temporal.TimeZone.from('Europe/Rome');
+        const tzGB = Temporal.TimeZone.from('Europe/London');
+
+        const now = Temporal.Now.instant().toString();
+
+        const PKTime = tzPK.getPlainDateTimeFor(now).withCalendar('islamic');
+        const ITTime = tzIT.getPlainDateTimeFor(now).withCalendar('islamic');
+        const GBTime = tzGB.getPlainDateTimeFor(now).withCalendar('islamic');
+
+        const timeStrings = {
+            PK: this._formatDateTemporalToString(PKTime), 
+            IT: this._formatDateTemporalToString(ITTime), 
+            GB: this._formatDateTemporalToString(GBTime)
+        }
+
+        return timeStrings;
+    }
+
     _getTimeBoard() {
         const timeStrings = this._getTimeStrings();
+        const islamicTimeStrings = this._getTimeStringsIslamic();
+
         const timeBoard = `
-🇵🇰: ${timeStrings.PK}
-🇮🇹: ${timeStrings.IT}
-🇬🇧: ${timeStrings.GB}
+🇵🇰: ✝️ ${timeStrings.PK} - ☪️ ${islamicTimeStrings.PK}
+🇮🇹: ✝️ ${timeStrings.IT} - ☪️ ${islamicTimeStrings.IT}
+🇬🇧: ✝️ ${timeStrings.GB} - ☪️ ${islamicTimeStrings.GB}
 `;
 
         return timeBoard;
